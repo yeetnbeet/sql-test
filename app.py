d6a989e9c16ad0c1b6f522780539a25ca60c980c
yeetnbeet <segalcsam@gmail.com>
2023-01-25 15:21:12 -0700

    u

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ def query():
     return str(ll)
 
 if __name__ == "__main__":
-    app.run(debug=True,host=0.0.0.0,port=int(os.environ.get("PORT", 8080)))
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
